9460e7bb83b3919a5b20d7054edf856565b06a85
andru <andrushkoanna2003@gmail.com>
2022-11-01 21:14:49 +0300

    java

---
+++ src/main/java/by/grsu/aandrushko/todolist/db/dao/impl/TeamDaoImpl.java
@@ -0,0 +1,104 @@
+package by.grsu.aandrushko.todolist.db.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import by.grsu.aandrushko.todolist.db.dao.AbstractDao;
+import by.grsu.aandrushko.todolist.db.dao.IDao;
+import by.grsu.aandrushko.todolist.db.model.Team;
+
+public class TeamDaoImpl extends AbstractDao implements IDao<Integer, Team>{
+	
+	public static final TeamDaoImpl INSTANCE = new TeamDaoImpl();
+	
+	private TeamDaoImpl() {
+		super();
+	}
+	
+	@Override
+	public void insert(Team entity) {
+		try (Connection c = createConnection()){
+			PreparedStatement pstmt =c.prepareStatement("insert into team(name, number_of_part) values(?,?)");
+			pstmt.setString(1, entity.getName());
+			pstmt.setInt(2, entity.getNumberOfPart());
+			pstmt.executeUpdate();
+			entity.setId(getGeneratedId(c, "team"));
+		} catch (SQLException e) {
+			throw new RuntimeException("can't insert Team entity", e);
+		}
+	}
+	
+	@Override
+	public void update(Team entity) {
+		try (Connection c = createConnection()){
+			PreparedStatement pstmt =c.prepareStatement("update team set name=?, number_of_part=? where id=?");
+			pstmt.setString(1, entity.getName());
+			pstmt.setInt(2, entity.getNumberOfPart());
+			pstmt.setInt(3, entity.getId());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException("can't update Team entity", e);
+		}
+	}
+	
+	@Override
+	public void delete(Integer id) {
+		try (Connection c = createConnection()){
+			PreparedStatement pstmt =c.prepareStatement("delete from team where id=?");
+			pstmt.setInt(1, id);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException("can't delete Team entity", e);
+		}
+	}
+	
+	@Override
+	public Team getById(Integer id) {
+		Team entity = null;
+		try (Connection c = createConnection()){
+			PreparedStatement pstmt =c.prepareStatement("select * from team where id=?");
+			pstmt.setInt(1, id);
+			
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				entity = rowToEntity(rs);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException("can't get Team entity by id", e);
+		}
+		
+		return entity;
+			}
+	
+	@Override
+	public List<Team> getAll() {
+		List<Team> entitiesList = new ArrayList<>();
+		try (Connection c = createConnection()){
+			ResultSet rs = c.createStatement().executeQuery("select * from team");
+			while (rs.next()) {
+				Team entity = rowToEntity(rs);
+				entitiesList.add(entity);
+		} 
+	}
+		catch (SQLException e) {
+			throw new RuntimeException("can't select Team entities", e);
+		}
+		
+		return entitiesList;
+	}
+	
+	private Team rowToEntity(ResultSet rs) throws SQLException{
+		Team entity = new Team();
+		entity.setId(rs.getInt("id"));
+		entity.setName(rs.getString("name"));
+		entity.setNumberOfPart(rs.getInt("number_of_part"));
+		return entity;
+		
+	}
+}
---
+++ src/test/java/by/grsu/aandrushko/todolist/db/dao/impl/TeamDaoTest.java
@@ -0,0 +1,77 @@
+package by.grsu.aandrushko.todolist.db.dao.impl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import by.grsu.aandrushko.todolist.db.dao.IDao;
+import by.grsu.aandrushko.todolist.db.model.Team;
+
+public class TeamDaoTest extends AbstractTest {
+	private static final IDao<Integer, Team> dao = TeamDaoImpl.INSTANCE;
+	
+	@Test
+	public void testInsert() {
+		Team entity = new Team();
+		entity.setName("number1");
+		entity.setNumberOfPart(5);
+		dao.insert(entity);
+		Assertions.assertNotNull(entity.getId());
+	}
+	
+	@Test
+	public void testUpdate() {
+		Team entity = new Team();
+		entity.setName("number1");
+		entity.setNumberOfPart(5);
+		dao.insert(entity);
+		
+		String newName = "number_NEW";
+		entity.setName(newName);
+		Integer newNumberOfPart = 6;
+		entity.setNumberOfPart(newNumberOfPart);
+		dao.update(entity);
+		
+		Team updatedEntity = dao.getById(entity.getId());
+		Assertions.assertEquals(newName, updatedEntity.getName());
+		Assertions.assertEquals(newNumberOfPart, updatedEntity.getNumberOfPart());
+		Assertions.assertNotNull(updatedEntity);
+	}
+	
+	@Test
+	public void testDelete() {
+        Team entity = new Team();
+        entity.setName("number1");
+		entity.setNumberOfPart(5);
+		dao.insert(entity);
+		
+		dao.delete(entity.getId());
+		
+		Assertions.assertNull(dao.getById(entity.getId()));
+	}
+	
+	@Test
+	public void testGetById() {
+		Team entity = new Team();
+		entity.setName("number1");
+		entity.setNumberOfPart(5);
+		dao.insert(entity);
+		
+		Team selectEntity = dao.getById(entity.getId());
+		
+		Assertions.assertEquals(entity.getName(), selectEntity.getName());
+		Assertions.assertEquals(entity.getNumberOfPart(), selectEntity.getNumberOfPart());
+	}
+	
+	@Test
+	public void testGetAll() {
+		int expectedCount = getRandomNumber(1, 5);
+		for (int i = 1; i <= expectedCount; i++) {
+			Team entity = new Team();
+			entity.setName("number1" + i);
+			entity.setNumberOfPart(5);
+			dao.insert(entity);
+		}
+		
+		Assertions.assertEquals(expectedCount, dao.getAll().size());
+	}
+}
